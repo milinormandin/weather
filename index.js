7ea862418fe33a92f872798b1743992a7efb6ed0
milinormandin <99772427+milinormandin@users.noreply.github.com>
2022-03-01 23:55:24 +0000

    changed input to lat and long

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
     
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var city = String(req.body.cityInput);
-        console.log(req.body.cityInput);
+    // takes in latitude longitude
+        var lat = String(req.body.lat);
+        console.log(lat);
+
+          var lat = String(req.body.long);
+        console.log(lat);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
@@ -24,36 +27,36 @@ app.post("/", function(req, res) {
         const apiKey = "3ada98762763443f14a4af376b6cf373";
         const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&units=" + units + "&APPID=" + apiKey;
     
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
-        console.log(response.statusCode);
+    // // this gets the data from Open WeatherPI
+    // https.get(url, function(response){
+    //     console.log(response.statusCode);
         
-        // gets individual items from Open Weather API
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-          console.log(weatherData)
+    //     // gets individual items from Open Weather API
+    //     response.on("data", function(data){
+    //         const weatherData = JSON.parse(data);
+    //       console.log(weatherData)
           
-            const temp = weatherData.main.temp;
-            const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //         const temp = weatherData.main.temp;
+    //         const city = weatherData.name;
+    //         const weatherDescription = weatherData.weather[0].description;
+    //         const icon = weatherData.weather[0].icon;
+    //         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-          // getting humidity
-          const humidity = weatherData.main.humidity;
+    //       // getting humidity
+    //       const humidity = weatherData.main.humidity;
 
-          // getting windspeed
-          const windspeed = weatherData.wind.speed;
+    //       // getting windspeed
+    //       const windspeed = weatherData.wind.speed;
             
-            // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit<h2>");
-          // displating windspeed and humidity
-            res.write("<h2>The Humidity is " + humidity + " " + " the windspeed is  " + windspeed);
-            res.write("<br><img src=" + imageURL +">");
-            res.send();
-        });
-    });
+    //         // displays the output of the results
+    //         res.write("<h1> The weather is " + weatherDescription + "<h1>");
+    //         res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit<h2>");
+    //       // displating windspeed and humidity
+    //         res.write("<h2>The Humidity is " + humidity + " " + " the windspeed is  " + windspeed);
+    //         res.write("<br><img src=" + imageURL +">");
+    //         res.send();
+    //     });
+    // });
 })
 
 
