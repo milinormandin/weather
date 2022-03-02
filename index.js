7fb76884854180d151b6f2668fc2ab2c4c43a1fb
milinormandin <99772427+milinormandin@users.noreply.github.com>
2022-03-02 00:29:42 +0000

    added cloudiness to result page

--- index.js
+++ index.js
@@ -18,45 +18,53 @@ app.post("/", function(req, res) {
         var lat = String(req.body.lat);
         console.log(lat);
 
-          var lat = String(req.body.long);
-        console.log(lat);
+          var lon = String(req.body.lon);
+        console.log(lon);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
   // Using my api key
         const apiKey = "3ada98762763443f14a4af376b6cf373";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&units=" + units + "&APPID=" + apiKey;
+        const url = "https://api.openweathermap.org/data/2.5/weather?"+ "lat=" + lat + "&lon=" + lon + "&units=" + units + "&APPID=" + apiKey;
     
-    // // this gets the data from Open WeatherPI
-    // https.get(url, function(response){
-    //     console.log(response.statusCode);
+    // this gets the data from Open WeatherPI
+    https.get(url, function(response){
+        console.log(response.statusCode);
         
-    //     // gets individual items from Open Weather API
-    //     response.on("data", function(data){
-    //         const weatherData = JSON.parse(data);
-    //       console.log(weatherData)
+        // gets individual items from Open Weather API
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+          console.log(weatherData)
           
-    //         const temp = weatherData.main.temp;
-    //         const city = weatherData.name;
-    //         const weatherDescription = weatherData.weather[0].description;
-    //         const icon = weatherData.weather[0].icon;
-    //         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const temp = weatherData.main.temp;
+            const city = weatherData.name;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-    //       // getting humidity
-    //       const humidity = weatherData.main.humidity;
+          // getting humidity
+          const humidity = weatherData.main.humidity;
 
-    //       // getting windspeed
-    //       const windspeed = weatherData.wind.speed;
-            
-    //         // displays the output of the results
-    //         res.write("<h1> The weather is " + weatherDescription + "<h1>");
-    //         res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit<h2>");
-    //       // displating windspeed and humidity
-    //         res.write("<h2>The Humidity is " + humidity + " " + " the windspeed is  " + windspeed);
-    //         res.write("<br><img src=" + imageURL +">");
-    //         res.send();
-    //     });
-    // });
+          // getting windspeed
+          const windspeed = weatherData.wind.speed;
+
+          // getting cloudiness
+          const clouds = weatherData.clouds.all;
+          
+            // displays the output of the results
+            res.write("<h1> The weather is " + weatherDescription + "<h1>");
+            res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit<h2>");
+          // displating windspeed and humidity
+            res.write("<h2>The Humidity is " + humidity + " " + " and the windspeed is  " + windspeed);
+
+          // displating cloudiness
+            res.write("<h2>The Cloudiness is " + clouds + "%");
+
+          
+            res.write("<br><img src=" + imageURL +">");
+            res.send();
+        });
+    });
 })
 
 
